d4efa03002f588f7f11264712e7f28ada4b78de8
Vlad Sydorenko <vladyslav.sydorenko@gmail.com>
2016-09-13 12:03:13 +0300

    Update urls.py file to new syntax

--- kpibot/urls.py
+++ kpibot/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
-from request_handler import views
+from timetable import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
-)
+]
