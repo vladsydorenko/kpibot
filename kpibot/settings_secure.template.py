8db75a7e4048bca8b942216138b9e7e11a3e0469
Vlad Sydorenko <vsydorenko@rmn.com>
2017-09-01 18:33:15 +0300

    Added few fields to secure settings template

--- kpibot/settings_secure.template.py
+++ kpibot/settings_secure.template.py
@@ -1,3 +1,4 @@
 BOT_TOKEN = ""  # Telegram API token for KPIbot
-BOTAN_TOKEN = ""  # Botan API token
 SECRET_KEY = ""  # Django secret key
+ALLOWED_HOSTS = []
+LOG_CHAT_ID = 1234  # ID of admin chat. It will be used to send error messages.
